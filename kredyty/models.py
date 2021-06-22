898dee5fb88f1e2834af4f8eea3db8e6d34b5b2a
farix <marcin_krajewski@onet.eu>
2021-06-22 14:09:25 +0200

    dodawanie wnioskow przez admina

--- kredyty/models.py
+++ kredyty/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Wnioski(models.Model):
+    kredytobiorca = models.CharField(max_length=64)
+    kwota = models.PositiveIntegerField(validators=[MinValueValidator(200), MaxValueValidator(10000)])
+    okres = models.PositiveSmallIntegerField(default=6, validators=[MinValueValidator(1), MaxValueValidator(36)])
+
+    def __str__(self):
+        return 'kredyt na kwotę: ' + str(self.kwota)
