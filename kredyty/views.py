44a9993fea3958ba4f92ecc020656d22bf649822
farix <marcin_krajewski@onet.eu>
2021-06-25 16:02:34 +0200

    dodanie logowania

--- kredyty/views.py
+++ kredyty/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Wnioski
 from .forms import WnioskiForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -9,6 +10,7 @@ def lista_kredytow(request):
     wszystkie = Wnioski.objects.all()
     return render(request, 'kredyty.html', {'wnioski': wszystkie})
 
+@login_required
 def nowy_wniosek(request):
     form = WnioskiForm(request.POST or None, request.FILES or None)
 
@@ -17,6 +19,7 @@ def nowy_wniosek(request):
         return redirect(lista_kredytow)
     return render(request, 'wniosek_form.html', {'form': form})
 
+@login_required
 def edytuj_wniosek(request, id):
     wniosek = get_object_or_404(Wnioski, pk=id)
     form = WnioskiForm(request.POST or None, request.FILES or None,
@@ -27,6 +30,7 @@ def edytuj_wniosek(request, id):
         return redirect(lista_kredytow)
     return render(request, 'wniosek_form.html', {'form': form})
 
+@login_required
 def usun_wniosek(request, id):
     wniosek = get_object_or_404(Wnioski, pk=id)
 
