a785cb4b3c1b99be3fac9c456d4db6b5a12a8de7
farix <marcin_krajewski@onet.eu>
2021-07-09 15:41:30 +0200

    zastosowanie jsona do pozostalych pol modelu

--- kredyty/models.py
+++ kredyty/models.py
@@ -8,9 +8,10 @@ class Wnioski(models.Model):
     kredytobiorca = models.CharField(max_length=64)
     imie = models.CharField(max_length=64)
     nazwisko = models.CharField(max_length=64)
-    kwota = models.PositiveIntegerField(default=200, validators=[MinValueValidator(200),
-                                                                 MaxValueValidator(10000)])
-    okres = models.PositiveSmallIntegerField(default=6, validators=[MinValueValidator(1), MaxValueValidator(36)])
+    kwota = models.PositiveIntegerField(default=200, validators=[MinValueValidator(ConfigData().get_data()['min_kwota']),
+                                                                 MaxValueValidator(ConfigData().get_data()['max_kwota'])])
+    okres = models.PositiveSmallIntegerField(default=6, validators=[MinValueValidator(ConfigData().get_data()['min_okres']),
+                                                                    MaxValueValidator(ConfigData().get_data()['max_okres'])])
     oprocentowanie = models.FloatField(default=ConfigData().get_data()['oprocentowanie'],
                                        validators=[MinValueValidator(ConfigData().get_data()['oprocentowanie']),
                                                    MaxValueValidator(ConfigData().get_data()['oprocentowanie'])])
