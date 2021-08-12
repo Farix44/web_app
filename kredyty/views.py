4a244a74b71a2541bdc904eef6849387cd50b283
farix <marcin_krajewski@onet.eu>
2021-08-13 00:54:16 +0200

    test fix

--- kredyty/views.py
+++ kredyty/views.py
@@ -29,7 +29,6 @@ class LoansViewSet(viewsets.ModelViewSet):
     serializer_class = LoansSerializer
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
-    # test
 
     def create(self, request, *args, **kwargs):
         if CheckLoanValidators().validate() == True:
