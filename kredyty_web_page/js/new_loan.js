47d88cfdd6216e6462e61eed5df5469be1461208
farix <marcin_krajewski@onet.eu>
2021-08-19 18:31:16 +0200

    utworzenie finalnej strony do dodawania kredytow

---
+++ kredyty_web_page/js/new_loan.js
@@ -0,0 +1,36 @@
+var received_json;
+var rec_json_obj;
+var admin_token = '7c374722bf776133c827a966f9fc9d89fbde7c6f';
+
+// wysyłanie PUT zedytowanych danych z formularza
+function submitNewLoan() {
+    var data = getFormData();
+    var json = JSON.stringify(data);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'http://127.0.0.1:8000/kredyty/loans/', true);
+    xhr.setRequestHeader('Authorization', 'Token ' + admin_token);
+    xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+    xhr.onload = function () {
+        if (xhr.readyState == 4 && xhr.status == "200") {
+            console.table(users);
+        } else {
+            console.error(users);
+        }
+    }
+    xhr.send(json);
+}
+
+// pobieranie danych z formularza do obiektu
+function getFormData() {
+    var elements = document.getElementById("add_new_loan_form").elements;
+    var obj ={};
+    for(var i = 0 ; i < elements.length ; i++){
+        var item = elements.item(i);
+        obj[item.name] = item.value;
+    }
+    // console.log(JSON.stringify(obj));
+    return obj;
+}
+
+
