17ec975481ee87767ba0018099662155b80230dc
farix <marcin_krajewski@onet.eu>
2021-09-01 18:36:35 +0200

    zmiany po probach uruchomienia automatycznie skryptu (komentarze)

--- presetup-api.sh
+++ presetup-api.sh
@@ -3,6 +3,7 @@
 #if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
 #    (cd web_app; python manage.py createsuperuser --no-input)
 #fi
+#python manage.py runserver 0.0.0.0:8000
 python manage.py makemigrations
 python manage.py migrate
 DJANGO_SUPERUSER_PASSWORD=admin \
